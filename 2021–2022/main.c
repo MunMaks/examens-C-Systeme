9ecb21f7661a3d45303338e196083b8515a5dc99
Maks Munait <98954111+MunMaks@users.noreply.github.com>
2024-05-12 03:28:12 +0200

    Create main.c

---
+++ 2021–2022/main.c
@@ -0,0 +1,59 @@
+/*
+    Préparé par MUNAITPASOV M.
+    Université Gustave Eiffel
+
+    Pour trouver la question souhaitée, cherchez par [Qn], avec n = {1, ..., 15}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+[Q1] Lister les éventuels effets secondaires et donner la valeur de l’expression
+    sous-jacente à chacune des instructions du morceau de programme suivant.
+
+    int i, j, *p;
+    i = 99;
+    i = printf("%d\n", i);
+    j = ++i;
+    i = j = j + 5;
+    p = (int *) malloc(sizeof(int) * i);
+    p[j < 8] = 32;
+    printf("%d\n", p[0]);
+    p[1] = i == j;
+    free(p);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
